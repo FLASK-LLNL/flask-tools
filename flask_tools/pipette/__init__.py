b7c0753b7c544bd3560e5684898591e48c50d6ba
Peggy Li <li54@llnl.gov>
2026-06-24 12:42:42 -0700

    Add header

--- flask_tools/pipette/__init__.py
+++ flask_tools/pipette/__init__.py
@@ -1,3 +1,10 @@
+###############################################################################
+## Copyright 2025-2026 Lawrence Livermore National Security, LLC.
+## See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+###############################################################################
+
 from .grade_rxn import grade_reaction
 from .constants import FinalGrade, ReactionGrade, ToolResult, ToolStatus
 
--- flask_tools/pipette/grade_rxn.py
+++ flask_tools/pipette/grade_rxn.py
@@ -1,3 +1,10 @@
+###############################################################################
+## Copyright 2025-2026 Lawrence Livermore National Security, LLC.
+## See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+###############################################################################
+
 """
 A CLI tool and main entry point to pipette. See pipette/README.md for more details.
 
